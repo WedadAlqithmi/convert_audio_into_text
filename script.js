c7c90fafc1a18b6e0f7a63ef7e6caf69cac1e100
WedadAlqithmi <108210044+WedadAlqithmi@users.noreply.github.com>
2022-07-20 20:15:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+var speechRecognition = window.webkitSpeechRecognition
+
+var recognition = new speechRecognition()
+
+var textbox = $("#textbox")
+
+var insturctions = $("#insturctions")
+// empty 
+var content = ''
+// if the recording start then recognise the audio to convert it 
+recognition.continuos = true 
+
+
+recognition.onstart = function() {
+    insturctions.text("voice recognition is on ")
+}
+
+// if there is no speech ant more the function will stop 
+recognition.onspeechend = function() {
+    insturctions.text("No Activity ")
+}
+
+// if an error occured an error message should appear 
+recognition.onerror = function(){
+    insturctions.text("try again")
+}
+
+//while recording the audio 
+recognition.onresult = function (event) {
+    var current = event.resultIndex;
+
+    var transcript = event.results[current][0].transcript
+
+    content += transcript 
+
+    textbox.val(content)
+}
+
+//main function 
+$("#start-btn").click(function(event){
+    if (content.length){
+        content+= ''
+    }
+    recognition.start() 
+
+})
